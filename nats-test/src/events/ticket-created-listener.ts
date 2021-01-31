6b6a3d3c115d37bcb0d7fc4751722654a83474af
moeinD <karimi.mohammad1977@gmail.com>
2021-01-31 13:32:42 +1000

    some more refactoring and add the some common section to be used later in other services.

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,13 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
-export class TicketCreatedLister extends Listener {
-    subject = 'ticket:created';
+import { TicketCreatedEvent } from './ticket-created-events';
+import { Subjects } from './subjects';
+
+export class TicketCreatedLister extends Listener<TicketCreatedEvent> {
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payment-service';
-    onMessage(data: any, msg: Message): void {
-        console.log('Event data ', data);
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+        console.log('Event data ', data.id, data.price, data.title);
         msg.ack();
     }
 
