313b016098cc7598d8e90b0d9ccfe5949f34fad3
moeinD <karimi.mohammad1977@gmail.com>
2021-02-08 22:07:04 +1000

    Add the new test for checking if the ticket has been reserved by another order we can not use it for the new order and got the 400 error.

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import request from "supertest";
 import { app } from "../../app";
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
 
 
 
@@ -14,6 +16,32 @@ it('returns an error if the ticket does not exist ', async () => {
 })
 
 it('returns an error if the ticket is already reserved ', async () => {
+    /**first we need to create a ticket and save it in the database */
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save();
+
+    /**we need to create a new order and
+     *  asociated the order with that ticket
+     */
+    const order = Order.build({
+        ticket,
+        userId: 'test',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    })
+    await order.save();
+    /**now if we try create a new order and try to use
+     * this ticket will got the error
+     */
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id }).expect(400);
+
 
 })
 
