462bf72ed81bad3705be968da110b02e19bd70d4
moeinD <karimi.mohammad1977@gmail.com>
2021-02-08 22:24:13 +1000

    Add the new section for getting the list of the orders for the user and associated the list of the ticket with as well.

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from 'express';
+import { requireAuth } from '@tmfyticket/common';
+import { Order } from '../models/order';
+
+
+
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+    /**populate will asociate the ticket with the order */
+    const orders = await Order.find({
+        userId: req.currentUser!.id
+    }).populate('ticket');
+
+    res.send(orders);
 })
 
 export { router as indexOrderRouter };
