a696e184fb6af7d9aa1d8042536a68293898509a
moeinD <karimi.mohammad1977@gmail.com>
2021-02-10 08:17:11 +1000

    Add the new section for deleting that cancel the orde and check the order and if we do not have it pass the not found and if we do not have the auth pass not authorized to the user.

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@tmfyticket/common';
+
 
 const router = express.Router();
 
-router.delete('/api/orders:orderId', async (req: Request, res: Response) => {
-    res.send({});
+router.delete('/api/orders:orderId', requireAuth, async (req: Request, res: Response) => {
+    /**we do not want to delete the order and
+     * just want to cancel the order
+     */
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    /**check if we do not have the order send the not found  */
+    if (!order)
+        throw new NotFoundError();
+    /**check if the order request by the not the owner  */
+    if (order.userId !== req.currentUser!.id)
+        throw new NotAuthorizedError();
+    order.status = OrderStatus.Canceled;
+    await order.save();
+    res.status(204).send(order);
 })
 
 export { router as deleteOrderRouter };
