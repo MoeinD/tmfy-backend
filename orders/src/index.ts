d819648c00cacd29d5b96027e39df20e355cc351
moeinD <karimi.mohammad1977@gmail.com>
2021-02-13 19:58:10 +1000

    add the ticket create listerner and ticket update listener to the main index file in the order service.

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listenters/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listenters/ticket-updated-listener';
+
 
 
 const start = async () => {
@@ -33,6 +36,9 @@ const start = async () => {
         })
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new TicketCreatedListener(natsWrapper.client).listen();
+        new TicketUpdatedListener(natsWrapper.client).listen();
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
