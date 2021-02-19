66cc8cf0a416b0234b56d81e9d482e11fc21197f
moeinD <karimi.mohammad1977@gmail.com>
2021-02-19 20:03:30 +1000

    some more refactoring to fix the issue for the version.

--- orders/src/events/listenters/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listenters/__test__/ticket-updated-listener.test.ts
@@ -1,39 +1,42 @@
-import { TicketUpdatedListener } from "../ticket-updated-listener";
-import { natsWrapper } from "../../../nats-wrapper";
 import mongoose from 'mongoose';
-import { Ticket } from "../../../models/ticket";
-import { TicketUpdatedEvent } from "@tmfyticket/common";
-import { Message } from "node-nats-streaming";
+import { Message } from 'node-nats-streaming';
+import { TicketUpdatedEvent } from '@tmfyticket/common';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
 
 const setup = async () => {
-    //Create a listener
+    // Create a listener
     const listener = new TicketUpdatedListener(natsWrapper.client);
-    //Create and save a ticket
+
+    // Create and save a ticket
     const ticket = Ticket.build({
         id: mongoose.Types.ObjectId().toHexString(),
-        title: 'test',
-        price: 40
-    })
-    ticket.save();
-    console.log(ticket);
-    //Create a fake data object
+        title: 'concert',
+        price: 20,
+    });
+    await ticket.save();
+
+    // Create a fake data object
     const data: TicketUpdatedEvent['data'] = {
         id: ticket.id,
-        price: 56,
-        title: 'test2',
         version: ticket.version + 1,
-        userId: mongoose.Types.ObjectId().toHexString(),
-    }
-    //Create a fake message object
-    //@ts-ignore
-    const msg: Message = {
-        ack: jest.fn()
-    }
-    //return all of this stuff
-    return { data, listener, msg, ticket };
-}
+        title: 'new concert',
+        price: 999,
+        userId: 'ablskdjf',
+    };
 
-it('finds, updates and save a ticket ', async () => {
+    // Create a fake msg object
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn(),
+    };
+
+    // return all of this stuff
+    return { msg, data, ticket, listener };
+};
+
+it('finds, updates, and saves a ticket', async () => {
     const { msg, data, ticket, listener } = await setup();
 
     await listener.onMessage(data, msg);
@@ -43,8 +46,12 @@ it('finds, updates and save a ticket ', async () => {
     expect(updatedTicket!.title).toEqual(data.title);
     expect(updatedTicket!.price).toEqual(data.price);
     expect(updatedTicket!.version).toEqual(data.version);
-})
+});
 
-it('aks the message', () => {
+it('acks the message', async () => {
+    const { msg, data, listener } = await setup();
 
-})
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+});
