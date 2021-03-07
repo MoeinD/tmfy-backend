786e6e7bb5d11e68e0454a8c9fc7c23ec3d7fcf6
moeinD <karimi.mohammad1977@gmail.com>
2021-03-07 18:39:44 +1000

    Add the new logic that user can do the order by passing the ticketId.

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,8 +1,21 @@
+import useRequest from "../../hooks/use-request";
+
 const TicketShow = ({ ticket }) => {
+
+    const { doRequest, errors } = useRequest({
+        url: '/api/orders',
+        method: 'post',
+        body: {
+            ticketId: ticket.id
+        },
+        onSuccess: (order) => console.log('tis is the order ', order)
+    })
     return <div>
         <h1>{ticket.title}</h1>
         <h4>{ticket.price}</h4>
-        <button className="btn btn-primary" >Purchase</button>
+        <p>{ticket.orderId}</p>
+        {errors}
+        <button className="btn btn-primary" onClick={doRequest} >Purchase</button>
     </div>
 };
 
