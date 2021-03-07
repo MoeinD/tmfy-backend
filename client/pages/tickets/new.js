8436387754d5d068484df2f70aa27bbd8a59962d
moeinD <karimi.mohammad1977@gmail.com>
2021-03-07 10:27:04 +1000

    Add the setting and getter for the state on the new form of ticket.

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -7,12 +7,11 @@ const NewTicket = () => {
     const onBlur = () => {
         const value = parseFloat(price);
 
-        if (isNaN(value)) {
-            return;
-        }
+        if (isNaN(value)) return;
 
         setPrice(value.toFixed(2));
-    };
+
+    }
 
     return (
         <div>
@@ -20,25 +19,16 @@ const NewTicket = () => {
             <form>
                 <div className="form-group">
                     <label>Title</label>
-                    <input
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="form-control"
-                    />
+                    <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" />
                 </div>
                 <div className="form-group">
                     <label>Price</label>
-                    <input
-                        value={price}
-                        onBlur={onBlur}
-                        onChange={(e) => setPrice(e.target.value)}
-                        className="form-control"
-                    />
+                    <input value={price} onBlur={onBlur} onChange={(e) => setPrice(e.target.value)} className="form-control" />
                 </div>
                 <button className="btn btn-primary">Submit</button>
             </form>
         </div>
-    );
-};
+    )
+}
 
 export default NewTicket;
